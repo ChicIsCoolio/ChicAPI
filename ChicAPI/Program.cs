882f0b8a8a67be497856231c68ce40450c764cb2
ChicIsCoolio <chicfromfn@gmail.com>
2021-02-07 15:23:26 +0100

    sid

--- ChicAPI/Program.cs
+++ ChicAPI/Program.cs
@@ -1,10 +1,12 @@
 using EpicGames.NET;
+using EpicGames.NET.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -13,10 +15,21 @@ namespace ChicAPI
 {
     public class Program
     {
+        public static string Root = "/home/runner/ChicAPI/";
+
         public static EpicServices Epic;
 
         public static void Main(string[] args)
         {
+            Root = "C:\\Users\\Hopík\\source\\repos\\ChicAPI\\";
+
+            Console.WriteLine(OAuthService.LoginURL);
+            Epic = new EpicServices(GetSid(), OAuthService.AuthTokenType.LAUNCHER);
+
+            Console.WriteLine(Epic.Account.DisplayName);
+
+            Epic.KillSession();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -27,5 +40,21 @@ namespace ChicAPI
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseUrls("http://0.0.0.0:8080");
                 });
+    
+        private static string GetSid()
+        {
+            using (var stream = File.CreateText($"{Root}input"))
+            {
+                stream.WriteLine("sid=[INSERT SID HERE]");
+            }
+
+            Console.WriteLine("Insert sid and press enter");
+            Console.ReadKey();
+
+            string sid = File.ReadAllText($"{Root}input").Split('=')[1];
+            File.Delete($"{Root}input");
+
+            return sid;
+        }
     }
 }
