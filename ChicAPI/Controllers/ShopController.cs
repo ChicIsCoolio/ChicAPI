49714a85916a443a79e32dc4cec4a0af440050c5
Chic <chicfromfn@gmail.com>
2021-02-08 21:25:06 +0000

    pog?

--- ChicAPI/Controllers/ShopController.cs
+++ ChicAPI/Controllers/ShopController.cs
@@ -32,19 +32,28 @@ namespace ChicAPI.Controllers
         Catalog GetCatalog()
         {
             if (HasCatalogInCache(out Catalog catalog)) return catalog;
-            else return Program.Epic.GetCatalog();
+            else 
+            {
+                Console.WriteLine("Something");
+
+                catalog = Program.Epic.GetCatalog();
+                Program.SaveToCache(JsonConvert.SerializeObject(catalog, Formatting.Indented), $"RawShop_{catalog.Expiration.ToString().Replace(' ', '_').Replace(":", ".").Replace("/", "-")}");
+                return catalog;
+            }
         }
 
         bool HasCatalogInCache(out Catalog catalog)
         {
             catalog = new Catalog();
 
+            if (!Program.ListCache().Any(x => x.Contains("RawShop_"))) return false;
+
             var date = Program.ListCache().Where(x => x.Contains("RawShop_"))
-                .Max(x => DateTime.Parse(x.Replace("RawShop_", "")));
+                .Max(x => DateTime.Parse(x.Split("RawShop_")[1].Replace('_', ' ').Replace('.', ':').Replace('-', '/')));
 
             if (date - DateTime.UtcNow > TimeSpan.Zero)
             {
-                catalog = JsonConvert.DeserializeObject<Catalog>(Program.LoadFromCache($"RawShop_{date.ToString("o")}"));
+                catalog = JsonConvert.DeserializeObject<Catalog>(Program.LoadFromCache($"RawShop_{date.ToString().Replace(' ', '_').Replace(":", ".").Replace("/", "-")}"));
                 return true;
             }
             else return false;
